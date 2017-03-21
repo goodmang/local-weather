090c0cc02bfc019fd1832011bf349f872054fe72
Greg Goodman <greg@goodmandigital.com>
2017-03-21 00:16:55 -0700

    Switched get city API

--- js/ggapp.weather.js
+++ js/ggapp.weather.js
@@ -1,47 +1,44 @@
+'use strict';
+
 var coordinates;
 var isCelcius = true;
 
 function switchUOM() {
-  var tempElem = document.getElementById("temp");
-  var temp = tempElem.textContent;
+    var tempElem = document.getElementById("temp");
+    var temp = tempElem.textContent;
 
-  uomElem = document.getElementById("uom");
+    uomElem = document.getElementById("uom");
 
-  isCelcius = !isCelcius;
-  if (isCelcius) {
-    temp = (temp - 32) / 9 * 5;
-    uomElem.innerHTML = 'C';
-  } else {
-    temp = temp * 9 / 5 + 32;
-    uomElem.innerHTML = 'F';
-  }
-  tempElem.innerHTML = temp + "°";
+    isCelcius = !isCelcius;
+    if (isCelcius) {
+        temp = (temp - 32) / 9 * 5;
+        uomElem.innerHTML = 'C';
+    } else {
+        temp = temp * 9 / 5 + 32;
+        uomElem.innerHTML = 'F';
+    }
+    tempElem.innerHTML = temp + "°";
 }
 
 function getCity(position) {
     coordinates = position;
     $.ajax({
-        url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude + '&sensor=false',
+        url: "http://ipinfo.io/json",
         success: function(result) {
             var elem = document.getElementById("city");
-
-            if (result.status == "OK") {
-                elem.innerHTML = "<h3>" + result.results[1].formatted_address; + "</h3>";
-                getWeather();
-            } else {
-                elem.innerHTML = '<h3 class="text-danger">City not found</h3>';
-            }
+            elem.innerHTML = "<h3>" + result.city + ", " + result.region + "</h3>";
+            getWeather();
         }
     });
 }
 
 function convertTemp(temp) {
-  // temp is in Kelvin
-  if (isCelcius) {
-    return temp - 273.15;
-  } else {
-    return (temp - 273.15 + 32) * 9 / 5;
-  }
+    // temp is in Kelvin
+    if (isCelcius) {
+        return temp - 273.15;
+    } else {
+        return (temp - 273.15 + 32) * 9 / 5;
+    }
 }
 
 function getWeather() {
@@ -56,11 +53,11 @@ function getWeather() {
             elem.innerHTML = "C";
 
             // Description
-            var elem = document.getElementById("description");
+            elem = document.getElementById("description");
             elem.innerHTML = result.weather[0].description;
 
             // Icon
-            var elem = document.getElementById("weather-icon");
+            elem = document.getElementById("weather-icon");
             var icon = result.weather[0].icon;
             elem.innerHTML = '<img src="http://openweathermap.org/img/w/' + icon + '.png">';
         }
@@ -69,11 +66,11 @@ function getWeather() {
 }
 
 function init() {
-  var elem;
+    var elem;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getCity);
         elem = document.getElementById("uom");
-         $("#uom").on("click", switchUOM);
+        $("#uom").on("click", switchUOM);
     } else {
         elem = document.getElementById("description");
         elem.innerHTML = '<p class="text-danger">Geolocation is not supported by this browser.  Sorry no weather. :(';
